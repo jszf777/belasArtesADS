bac01b44ff3ff492223b2d4848d5c281366078e8
Jade <106936120+jszf777@users.noreply.github.com>
2023-04-20 12:33:37 -0300

    Update p_pares.py

--- EDNL/pilhas/p_pares.py
+++ EDNL/pilhas/p_pares.py
@@ -1,18 +1,6 @@
-'''2. Desenvolva um algoritmo que receba do usuário 8 números inteiros e construa uma pilha apenas com os números pares.
+'''Desenvolva um algoritmo que receba do usuário 8 números inteiros e construa uma pilha apenas com os números pares.
 Algoritmo pilha_pares
-Inicio
-	pp[8] Pilha;
-	x,pi_int Inteiro;
-	
-	//empilhando na Pilha pp, 8 números digitados pelo usuário
-	para x = 1 até 8 passo +1 faça
-		pi_int = ler(mostrar ("digite um inteiro));
-		Se(pi_int mod 2 == 0)//Se (pi_int for par)
-			Então pp.Empilhar(pi_int);
-	fimpara
-		
-	pp.mostrar( );
-fim'''
+'''
 
 pp = [];
 
